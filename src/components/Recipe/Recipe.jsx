aa612314f818630e12e11119b18203405195c855
Mohammed-Saiful-Islam <mosai21498@gmail.com>
2024-03-17 22:23:39 +0600

    section recipes some code added

--- src/components/Recipe/Recipe.jsx
+++ src/components/Recipe/Recipe.jsx
@@ -1,9 +1,26 @@
+import TimeIcon from '../../assets/time.svg';
+import FireIcon from '../../assets/fire.svg';
+import PropTypes from 'prop-types';
 
-const Recipe = ({recipe}) => {
+const Recipe = ({ recipe }) => {
     console.log(recipe);
+    const { img, title, details, ingredients, time, calories } = recipe;
     return (
         <div>
-            
+            <div>
+                <img src={img} alt="" />
+                <h1 className='text-3xl font-bold'>{title}
+                </h1>
+                <p className='text-2xl font-semibold'>
+                    {details}</p>
+                <hr />
+                <p className='text-xl font-medium'>{ingredients}</p>
+                <hr />
+                <div className='flex justify-between'>
+                    <div className='flex space-x-3 text-xl font-medium'><img src={TimeIcon} alt="" /> <p>{time}</p></div>
+                    <div className='flex space-x-3 text-xl font-medium'><img src={FireIcon} alt="" /><p>{calories}</p></div>
+                </div>
+            </div>
         </div>
     );
 };
--- src/components/Recipes/Recipes.jsx
+++ src/components/Recipes/Recipes.jsx
@@ -1,23 +1,30 @@
 import { useEffect } from "react";
 import { useState } from "react";
 import Recipe from "../Recipe/Recipe";
+import PropTypes from 'prop-types'
 
 const Recipes = () => {
-    const[recipes,setRecipes]=useState([]);
-    useEffect(()=>{
+    const [recipes, setRecipes] = useState([]);
+    useEffect(() => {
         fetch('recipes.json')
-        .then(res=>res.json())
-        .then(data=>setRecipes(data))
+            .then(res => res.json())
+            .then(data => setRecipes(data))
         console.log(recipes);
-    },[])
+    }, [])
     return (
-        <div className="md:w-2/3">
-            <h1 className="text-4xl">Recipes: {recipes.length}</h1>
-            {
-                recipes.map(recipe=><Recipe key={recipe.id} recipe={recipe}></Recipe>)
-            }
+        <div className="w-[90%] max-w-6xl my-0 mx-auto">
+            <div className="md:w-[60%]">
+                <h1 className="text-4xl">Recipes: {recipes.length}</h1>
+                {
+                    recipes.map(recipe => <Recipe key={recipe.id} recipe={recipe}></Recipe>)
+                }
+            </div>
         </div>
     );
 };
+
+Recipes.propTypes = {
+
+}
 
 export default Recipes;
