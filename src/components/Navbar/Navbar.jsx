6cc284f960c7555c87e850c6988b8d07e491be86
Mohammed-Saiful-Islam <mosai21498@gmail.com>
2024-03-17 15:54:26 +0600

    navbar added

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,27 @@
+import SearchIcon from '../../assets/search.svg';
+import PersonIcon from '../../assets/person.svg';
+const Navbar = () => {
+    return (
+        <div className='w-[90%] max-w-6xl my-0 mx-auto mt-2 md:mt-6 '>
+            <div className='flex justify-between items-center flex-col md:flex-row space-x-2'>
+                <div>
+                    <h1 className='text-2xl md:text-4xl font-bold'>
+                        Recipe Calories
+                    </h1>
+                </div>
+                <div className='space-x-2 md:space-x-6'>
+                    <a className='hover:underline' href="">Home</a>
+                    <a className='hover:underline' href="">Recipes</a>
+                    <a className='hover:underline' href="">About</a>
+                    <a className='hover:underline' href="">Search</a>
+                </div>
+                <div className='flex items-center space-x-2 md:space-x-6'>
+                    <div className='flex items-center border-2 border-black border-solid p-2 rounded-xl'><a href=""><img className='min-w-8' src={SearchIcon} alt="" />
+                    </a> <input className='border-none' type="text" /></div>
+                    <div className='rounded-full '><a href=""><img className='min-w-8' src={PersonIcon} alt="" /></a></div>
+                </div>
+            </div>
+        </div>
+    );
+};
+export default Navbar;
--- tailwind.config.js
+++ tailwind.config.js
@@ -8,5 +8,12 @@ export default {
     extend: {},
   },
   plugins: [],
-}
+};
+// module.exports = {
+//   content: [
+//     'node_modules/daisyui/dist/**/*.js',
+//     'node_modules/react-daisyui/dist/**/*.js',
+//   ],
+//   plugins: [require('daisyui')],
+// }
 
